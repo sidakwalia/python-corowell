9c3204155b5459b0388adfabc6b15571828c30db
inderpreet.singh <inderpreet.singh@algoscale.com>
2022-07-21 16:54:33 +0530

    Added

--- app.py
+++ app.py
@@ -117,20 +117,23 @@ def result():
             updated=db.table('test_details').where("email_id",email_id).update({"serial_number":serial_number,"time_of_test":d2,"survey_answers":user_answers})
             if updated==1:
                 response={ "data": "Updated", "status_code": 200,"covid_results":covid_results,"email_id":email_id,"patient_name":user_name,"time_of_test":d2}
-                return json.loads(response)
+                print(type(response))
+                return json.dumps(response)
         else:
             print("here")
             insertion=db.table('test_details').insert({"patient_name":user_name,"serial_number":serial_number,"time_of_test":d2,"survey_answers":user_answers,"covid_results":covid_results,"email_id":email_id})
             if insertion==1:
                 response={ "data": "Inserted", "status_code": 200,"covid_results":covid_results,"email_id":email_id,"patient_name":user_name,"time_of_test":d2}
-                return json.loads(response)
+                print(type(response))
+                return json.dumps(response)
             else:
                 response={"data":"Not inserted","status_code": 400}
-                return json.loads(response)
+                print(type(response))
+                return json.dumps(response)
     except Exception as e:        
         print("error is---",str(e))
         response={ "data": "serial code is enter already used", "status_code": 400}
-        return json.loads(response)
+        return json.dumps(response)
     
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0", port=5100)
